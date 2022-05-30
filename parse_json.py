240a042fbd7e6df35bef390d2375c9dfaebf0e4f
Mart Timuska <matimu@ttu.ee>
2022-05-30 12:21:16 +0200

    Update parse_json.py

--- parse_json.py
+++ parse_json.py
@@ -3,7 +3,16 @@ import json, requests
 url = 'http://upload.itcollege.ee/~aleksei/eksam.json'
 
 def parse_json(url):
-    
+
     # your code here
-    
+
+    course_code = 0
+
+    for course in json.loads(requests.get(url).text)['courses']:
+        if course['name'] == 'Scripting languages':
+            course_code = course['code']
+
     return course_code
+
+
+print(parse_json(url))
